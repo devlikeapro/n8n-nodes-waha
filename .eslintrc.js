b327042ee153a6754e02e6b79124ef0f799d1b77
devlikepro <waha@devlike.pro>
2024-09-04 13:35:55 +0700

    Init trigger

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
 			extends: ['plugin:n8n-nodes-base/nodes'],
 			rules: {
 				'n8n-nodes-base/node-execute-block-missing-continue-on-fail': 'off',
+				'n8n-nodes-base/node-filename-against-convention': 'off',
 				'n8n-nodes-base/node-resource-description-filename-against-convention': 'off',
 				'n8n-nodes-base/node-param-fixed-collection-type-unsorted-items': 'off',
 				'n8n-nodes-base/node-param-options-type-unsorted-items': 'off',
---
+++ nodes/WAHA/WAHATrigger.node.ts
@@ -0,0 +1,70 @@
+import type {
+	IDataObject,
+	IWebhookFunctions,
+	INodeType,
+	INodeTypeDescription,
+	IWebhookResponseData,
+} from 'n8n-workflow';
+
+
+export class WAHATrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'WAHA Trigger',
+		name: 'wahaTrigger',
+		icon: 'file:waha.svg',
+		group: ['trigger'],
+		version: 1,
+		description: 'Handle WAHA events via webhooks',
+		defaults: {
+			name: 'WAHA Trigger',
+		},
+		inputs: [],
+		outputs: ["main"],
+		credentials: [ ],
+		webhooks: [
+			{
+				name: 'default',
+				httpMethod: 'POST',
+				responseMode: 'onReceived',
+				path: 'webhook',
+			},
+		],
+		properties: [
+			{
+				displayName: 'Events',
+				name: 'events',
+				type: 'multiOptions',
+				required: true,
+				default: [],
+				description: 'The event to listen to',
+				// eslint-disable-next-line n8n-nodes-base/node-param-multi-options-type-unsorted-items
+				options: [
+					{
+						name: '*',
+						value: '*',
+						description: 'Any time any event is triggered (Wildcard Event)',
+					},
+				],
+			},
+		],
+	};
+
+	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
+		const bodyData = this.getBodyData();
+		const req = this.getRequestObject();
+
+		const events = this.getNodeParameter('events', []) as string[];
+
+		const eventType = bodyData.type as string | undefined;
+
+		if (eventType === undefined || (!events.includes('*') && !events.includes(eventType))) {
+			// If not eventType is defined or when one is defined, but we are not
+			// listening to it do not start the workflow.
+			return {};
+		}
+
+		return {
+			workflowData: [this.helpers.returnJsonArray(req.body as IDataObject)],
+		};
+	}
+}
